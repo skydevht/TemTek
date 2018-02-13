8a73bddbe798738514e08d265f727206e2b0727a
hash <skydhash@gmail.com>
2018-02-12 21:05:43 -0500

    Paj akèy
    Se plis makè yo mwen ajoute, poko gen fonksyonalite vrèman

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
 import Main from './pages/Main';
+import Home from './pages/Home';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home}/>
+          <Route path="/app" component={Main}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Home.css';
+
+class Home extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+  };
+
+  render() {
+    return (
+      <div className="Home">
+        <div className="row">
+          <div className="grid-6 grid--offset-3">
+            <h3 className="welcome">Byenvini nan</h3>
+            <h1 className="brand">TèmTèk</h1>
+            <p className="intro">TèmTèk se yon platfòm kote ou ap ka jwenn tradiksyon tout tèm teknolojik ki genyen ak definisyon. Se itilizatè tankou ou ki pwopoze yo e vote sou yo. Konsa tou ou menm ou kapab pwopoze tou e vote jodi a menm. Nou ap rettann kontribisyon pa ou nan mouvman an.</p>
+            <div className="txt--center"><a className="btn btn--large login">Kòmanse jodi a</a></div>
+          </div>
+        </div>
+        <nav>
+          <ul>
+            <li><a>Glosè</a></li>
+            <li><a>Enfo</a></li>
+            <li><a>Skydev</a></li>
+            <li><a>Experts Online</a></li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Home;
